486b91be6a7aa3e4137395bcefa41fe6a8691920
Rihabahmad06 <116387833+Rihabahmad06@users.noreply.github.com>
2022-10-27 21:32:58 +0530

    Add files via upload

---
+++ DoWhileLoo.java
@@ -0,0 +1,15 @@
+package helloworld.java;
+
+public class DoWhileLoo {
+
+	public static void main(String[] args) {
+	int x=1;
+	do {
+		System.out.println("value of x:"+x);
+		x++;
+		System.out.println("\n");
+	}while(x<11);
+
+	}
+
+}
